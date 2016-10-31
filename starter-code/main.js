e6a414d613a20a5f6fe0c4dfb9ca85420e32068d
Sean Allen Lewis <seanallenlewis@gmail.com>
2016-10-30 18:16:55 -0700

    created comparable statement

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,17 @@
 console.log("JS file is connected to HTML! Woo!")
+
+var cardOne = 'King';
+var cardTwo = 'Queen';
+var cardThree = 'King';
+var cardFour = 'Queen';
+
+var choiceOne = cardTwo; 
+var choiceTwo = cardFour;
+
+
+if (choiceOne === choiceTwo) {
+alert('You Found A Match!');
+} else {
+	alert('Sorry, try again.');
+
+}
