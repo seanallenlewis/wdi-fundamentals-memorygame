199b808d8cbcdc5518a7a8933b0065f562231605
Sean Allen Lewis <seanallenlewis@gmail.com>
2016-11-02 19:12:21 -0700

    create cards loop

--- starter-code/main.js
+++ starter-code/main.js
@@ -9,9 +9,25 @@ var choiceOne = cardTwo;
 var choiceTwo = cardFour;
 
 
+
+var createCards = function() {
+	var gameBoardId = document.getElementById('game-board');
+	var NumOfCards = y;
+
+	for (var x = 0, x < 4, x++){
+		var newCardItem = document.createElement('div');
+		newCardItem.className = 'card';
+		gameBoardId.appendChild(newCardItem);
+		
+	}
+};
+
+createCards();
+
+/* commented out 'if' statement
 if (choiceOne === choiceTwo) {
 alert('You Found A Match!');
 } else {
 	alert('Sorry, try again.');
+} */
 
-}
