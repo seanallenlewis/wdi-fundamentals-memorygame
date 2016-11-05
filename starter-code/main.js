666773ddd03a9da3092f0b29e4d5496a2e083495
Sean Allen Lewis <seanallenlewis@gmail.com>
2016-11-04 23:23:42 -0700

    complete version of assignment

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,28 +1,86 @@
+
 console.log("JS file is connected to HTML! Woo!")
 
-var cardOne = 'King';
+/*var cardOne = 'King';
 var cardTwo = 'Queen';
 var cardThree = 'King';
 var cardFour = 'Queen';
 
 var choiceOne = cardTwo; 
 var choiceTwo = cardFour;
+*/
+
+// Array to create card identities
+var cards = ['king', 'queen', 'queen', 'king', 'queen', 'queen', 'king', 'king'];
+
+// represents the cards in play
+var cardsInPlay = [];
 
 
 
-var createCards = function() {
-	var gameBoardId = document.getElementById('game-board');
-	var NumOfCards = y;
+// find the board and set it to a variable
+// so that we can add cards to it
+var board = document.getElementById('game-board');
+// function that will create your board
+function createBoard() {
+  // loop through your cards array to create card elements for your board
+  for (var i = 0; i < cards.length; i++) {
+    // create a div element which will be used as a card
+    var cardElement = document.createElement('div');
+    // add a class to the card element which will help link styling
+    cardElement.className = 'card';
+		// this will set the card's 'data-card' to be the element of the array
+    // data- attributes are meant to store data about an element that is not tied to a style.
+    // i.e. "king"
+    cardElement.setAttribute('data-card', cards[i]);
+		// when a card is clicked the function isTwoCards will be executed
+    cardElement.addEventListener('click', isTwoCards);
 
-	for (var x = 0, x < 4, x++){
-		var newCardItem = document.createElement('div');
-		newCardItem.className = 'card';
-		gameBoardId.appendChild(newCardItem);
-		
-	}
-};
+    // append the card to the board
+    board.appendChild(cardElement);
+    board.appendChild(cardElement);
+  }
 
-createCards();
+}
+
+//checks to see if there are cards in play
+function isTwoCards() {
+  // add card to array of cards being viewed
+  // 'this' is not covered in the pre-work but
+  // for now, just know it gives you access to the cardElement you click on
+  cardsInPlay.push(this.getAttribute('data-card'));
+	// show the card's image
+	console.log(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') === 'king') {
+	// show king image
+		this.innerHTML = "<img src='images/kingspades.png'>";
+		//else show queen image	
+		} else {
+		this.innerHTML = "<img src='images/queenhearts.png'>";
+		}
+
+  // if you have two cards in play check for a match
+  if (cardsInPlay.length === 2) {
+    // pass the cardsInPlay as an argument to isMatch function
+    isMatch(cardsInPlay);
+    // clear cards in play array for next try
+    cardsInPlay = [];
+  }
+}
+createBoard();
+
+function isMatch(cards) {
+  // alert winning message
+  if (cards[0] === cards[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+
+  }
+}
+
+
+
 
 /* commented out 'if' statement
 if (choiceOne === choiceTwo) {
